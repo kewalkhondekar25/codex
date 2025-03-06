f48a0243b0d36b6a0a964aea3c0e21536c056368
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-06 23:42:32 +0530

    feat: common middleware package added

--- apps/core-backend/app.ts
+++ apps/core-backend/app.ts
@@ -9,7 +9,8 @@ app.use(cors());
 //routes
 import healthRouter from "./routes/healthCheck.route";
 import projectRouter from "./routes/project.route";
-import authMiddleware from "./middlewares/auth.middleware";
+// import authMiddleware from "./middlewares/auth.middleware";
+import { authMiddleware } from "common/authMiddleware"
 
 app.use("/api/v1", healthRouter);
 app.use("/api/v1/project", authMiddleware, projectRouter)
